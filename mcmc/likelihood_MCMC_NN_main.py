e9212061e1845fc05faddcdb1d42048db5789d27
DennisNeumann97 <122468530+DennisNeumann97@users.noreply.github.com>
2024-07-19 16:46:51 +0200

    Update likelihood_MCMC_NN_main.py

--- mcmc/likelihood_MCMC_NN_main.py
+++ mcmc/likelihood_MCMC_NN_main.py
@@ -118,6 +118,10 @@ points, log_w, log_l = sampler.posterior()
 
 print('Took me ',np.round((tend-tstart)/60),' minutes.')
 
+# Create directory
+if not os.path.exists('./output_files/'+outdir):mkdir
+    os.makedirs('./output_files/'+outdir)
+  
 np.save('./output_files/'+outdir+'MCMC'+name+'_points', points)
 np.save('./output_files/'+outdir+'MCMC'+name+'_log_weights', log_w)
 np.save('./output_files/'+outdir+'MCMC'+name+'_log_likelihood', log_l)
